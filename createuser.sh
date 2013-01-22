de95e5235b96d45d13720c7d243d2b9e6cbb0bf8
ggonzalez <ggonzalez@ggonzalez-OptiPlex-Abiquo.(none)>
2013-01-22 18:15:25 +0100

    Updated for blobstorage

--- createuser.sh
+++ createuser.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+user={$1}
+
 curl --verbose -X POST "http://localhost/api/admin/enterprises/2/users" \
        -H "Content-type: application/vnd.abiquo.user+xml;version=2.4" \
        -H "Accept: application/vnd.abiquo.user+xml;version=2.4" \
-       -d @createuser2.xml \
+       -d @$1 \
        -u admin:xabiquo | xmlindent -nbe -f
